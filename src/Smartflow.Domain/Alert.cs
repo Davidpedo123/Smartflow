a03fca853b3011c49cacf9381a865137fd929691
JosueIsOffline <josuehernandez2314@gmail.com>
2025-12-04 20:45:45 -0400

    feat: add properties to class Alert

--- src/Smartflow.Domain/Alert.cs
+++ src/Smartflow.Domain/Alert.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Smartflow.Domain.Enums;
 
 namespace Smartflow.Domain
 {
   public class Alert
   {
+    public string Type { get; set; }
+    public AlertSeverity Severity { get; set; }
+    public double Value { get; set; }
+    public double Threshold { get; set; }
+    public DateTime Timestamp { get; set; }
     //WARNING: member names can be the same as their enclosing   
     // public void Alert()
     // {
     //     Console.WriteLine("Incluir logica de alerta aqui");
     // }
-
   }
 }
