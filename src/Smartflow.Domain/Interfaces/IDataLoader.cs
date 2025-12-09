adb0fa356c4e8b609eced2052d617fc65c8f1b6e
JosueIsOffline <josuehernandez2314@gmail.com>
2025-12-09 15:37:33 -0400

    fix: remove Message call property from Alert object Co-authored-by-josueHernandez

--- src/Smartflow.Domain/Interfaces/IDataLoader.cs
+++ src/Smartflow.Domain/Interfaces/IDataLoader.cs
@@ -1,8 +1,10 @@
+using Smartflow.Domain.Models;
+
 namespace Smartflow.Domain.Interfaces
 {
   public interface IDataLoader
   {
-    void Load(ProcessedData data, string path);
-    Task LoadAsync(ProcessedData data, string path);
+    void Load(ProcessedData data, string outputPath);
+    void LoadBatch(List<ProcessedData> dataList, string outputPath);
   }
 }
