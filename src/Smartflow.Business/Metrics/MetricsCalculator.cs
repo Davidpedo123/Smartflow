3ae6a7000c36618837cb090dd6908a40bba8b276
JosueIsOffline <josuehernandez2314@gmail.com>
2025-12-10 09:59:48 -0400

    feat: add measureParallel and comparePerformance methods

--- src/Smartflow.Business/Metrics/MetricsCalculator.cs
+++ src/Smartflow.Business/Metrics/MetricsCalculator.cs
@@ -28,6 +28,60 @@ public class MetricsCalculator
 
     return metrics;
   }
+ 
+  public Domain.Models.Metrics MeasureParallel(List<string> inputPaths)
+  {
+    Stopwatch stopwatch = Stopwatch.StartNew();
 
-  //TODO: implementar MesasureParallel y ComparePerformance methods
+    _coordinator.ProcessAllDataParallel(inputPaths);
+    stopwatch.Stop();
+
+    var metrics = new Domain.Models.Metrics
+    {
+      ParallelTime = stopwatch.Elapsed.TotalSeconds
+    };
+
+
+
+    Console.WriteLine($"Tiempo paralelo: {metrics.ParallelTime}");
+    Console.WriteLine($"Speedup: {metrics.Speedup}");
+    Console.WriteLine($"Eficiencia: {metrics.Efficiency}");
+    Console.WriteLine($"Sequentialtime: {metrics.SequentialTime}");
+
+    return metrics;
+  }
+
+  public Domain.Models.Metrics ComparePerformance(List<string> inputPaths, int threadCount)
+{
+    Console.WriteLine("\n==========================================");
+    Console.WriteLine("   INICIANDO COMPARACIÓN DE RENDIMIENTO   ");
+    Console.WriteLine("==========================================\n");
+ 
+    Console.WriteLine(">>> Ejecutando Modo Secuencial...");
+    var seqResult = MeasureSequential(inputPaths);
+
+    Console.WriteLine("\n>>> Ejecutando Modo Paralelo...");
+    var parResult = MeasureParallel(inputPaths);
+
+    var finalMetrics = new Domain.Models.Metrics
+    {
+        SequentialTime = seqResult.SequentialTime,
+        ParallelTime = parResult.ParallelTime
+    };
+
+    finalMetrics.Calculate(threadCount);
+    finalMetrics.ToJson();
+
+    Console.WriteLine("\n==========================================");
+    Console.WriteLine("          RESULTADOS FINALES              ");
+    Console.WriteLine("==========================================");
+    Console.WriteLine($"Tiempo Secuencial: {finalMetrics.SequentialTime:F4} s");
+    Console.WriteLine($"Tiempo Paralelo:   {finalMetrics.ParallelTime:F4} s");
+    Console.WriteLine($"------------------------------------------");
+    Console.WriteLine($"Speedup:           {finalMetrics.Speedup:F2}x  (Veces más rápido)");
+    Console.WriteLine($"Eficiencia:        {finalMetrics.Efficiency:P2} (Uso de recursos)");
+    Console.WriteLine("==========================================\n");
+
+    return finalMetrics;
+}
 }
